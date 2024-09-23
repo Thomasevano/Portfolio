aa6f7a59e62e987ef7a8713df9ac44a898795596
Thomasevano <thomas-evano@hotmail.fr>
2024-09-23 23:35:35 +0200

    feat: add i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,26 @@
+export const languages = {
+  en: 'English',
+  fr: 'Français',
+};
+
+export const defaultLang = 'en';
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.projects': 'Projects',
+    'nav.skills': 'Skills',
+    'title.projects': 'Projects',
+    'title.skills': 'Skills'
+  },
+  fr: {
+    'nav.home': 'Accueil',
+    'nav.about': 'À propos',
+    'nav.projects': 'Projets',
+    'nav.skills': 'Compétences',
+    'title.projects': 'Projets',
+    'title.skills': 'Compétences'
+  },
+} as const;
