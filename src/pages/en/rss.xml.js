ff940e0423ecad438eef81073225e0aefa034f76
Thomasevano <thomas-evano@hotmail.fr>
2024-11-25 17:15:42 +0100

    i18n: rss feed

--- src/pages/en/rss.xml.js
+++ src/pages/en/rss.xml.js
@@ -3,18 +3,18 @@ import { getCollection } from 'astro:content';
 
 export async function GET(context) {
   const posts = await getCollection("posts", (post) => {
-    return post.data.draft !== true && post.id.includes("fr/");
+    return post.data.draft !== true && post.id.includes("en/");
   });
   return rss({
-    title: 'Thomas Evano | Blog',
+    title: 'Thomas Evano | Blog | EN',
     description: 'Just a french software engineer adventures',
     site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/posts/${post.slug}/`,
+      link: `/en/posts/${post.slug}/`,
     })),
-    customData: `<language>fr-fr</language>`,
+    customData: `<language>en-us</language>`,
   });
 }
